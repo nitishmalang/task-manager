c3e03f18c930da1ee24881be5aab7692c6ff396f
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2023-06-26 17:47:28 +0530

    Create TaskManager.java

---
+++ TaskManager.java
@@ -0,0 +1,20 @@
+package com.example.taskmanager;
+
+import java.time.LocalDate;
+
+public class TaskManager {
+    public static void main(String[] args) {
+        // Create a Task instance
+        Task task = new Task("Sample Task", "This is a sample task", LocalDate.now());
+
+        // Perform operations on the task
+        System.out.println("Title: " + task.getTitle());
+        System.out.println("Description: " + task.getDescription());
+        System.out.println("Due Date: " + task.getDueDate());
+        System.out.println("Completed: " + task.isCompleted());
+
+        // Mark the task as completed
+        task.markAsCompleted();
+        System.out.println("Marked as Completed: " + task.isCompleted());
+    }
+}
